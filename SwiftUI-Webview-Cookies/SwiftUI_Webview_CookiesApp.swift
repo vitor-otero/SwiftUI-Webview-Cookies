50ebbc840d096548c77d60900804e39220b0cf46
Vitor Otero <votero.ext@gmv.com>
2023-03-26 14:58:43 +0100

    Initial Commit

---
+++ SwiftUI-Webview-Cookies/SwiftUI_Webview_CookiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Webview_CookiesApp.swift
+//  SwiftUI-Webview-Cookies
+//
+//  Created by Vítor Otero on 26/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Webview_CookiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
